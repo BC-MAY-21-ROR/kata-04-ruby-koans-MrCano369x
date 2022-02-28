d94af6c67c752d15f70a09dcfef3d5379a4b9544
MrCano369x <canorueda2000@gmail.com>
2022-02-28 13:24:31 -0600

    70/281 koans

---
+++ koans/lab.rb
@@ -0,0 +1,24 @@
+# array = [:peanut, :butter, :and, :jelly]
+# puts array[2,2].inspect
+# puts array[2,20].inspect
+# puts array[4,0].inspect
+# puts array[4,100].inspect
+# puts array[5,0].inspect
+
+# array = [:peanut, :butter, :and, :jelly]
+
+# puts array[2..-1].inspect
+
+# (2..-2).each do |x|
+#     puts x
+# end
+
+# hash = Hash.new {|hash, key| hash[key] = [] }
+
+#     # hash[:one] << "uno"
+#     # hash[:one] << "jjaa"
+#     # hash[:two] << "dos"
+# hash[:pinga]
+string = %(home\\cano)
+puts string.inspect
+puts string.length
